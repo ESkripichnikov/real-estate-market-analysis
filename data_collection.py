f7d6a7b2cd7fa5a152c6de77ff2ab2714706aa60
ESkripichnikov <skrijeka14@gmail.com>
2021-07-14 02:21:40 +0300

    links_parser corrected

---
+++ data_collection.py
@@ -0,0 +1,4 @@
+from parsers.apartments_links_parser import parse_apartments_links
+# from parsers.apartments_info_parser import
+
+parse_apartments_links()
--- parsers/apartments_links_parser.py
+++ parsers/apartments_links_parser.py
@@ -1,10 +1,10 @@
+import csv
 import requests
 from bs4 import BeautifulSoup
-from constants import secondary, primary, number_to_parse, cian_url, Category, Rooms
 from fake_useragent import UserAgent
 from tqdm import tqdm
-import csv
-
+from constants import secondary, primary, number_to_parse, cian_url, Category, Rooms
+import time
 
 ratio = number_to_parse / sum(primary + secondary)
 primary_count = [int(i * ratio) for i in primary]  # how many flats of each apartment type to parse
@@ -33,13 +33,16 @@ def get_apartments_links(page_soup) -> list[str]:
     try:
         apartments_links = page_soup.findAll('a', {"class": "_93444fe79c--link--39cNw"})
         apartments_links = [link.attrs["href"] for link in apartments_links]
+        if not apartments_links:
+            print("get no links!")
     except Exception:
+        print("get no links!")
         apartments_links = []
 
     return apartments_links
 
 
-def get_all_apartments_links():
+def parse_apartments_links():
     apartments_links = []
 
     for rooms, pages in tqdm(primary_pages.items()):
@@ -47,7 +50,8 @@ def get_all_apartments_links():
         for page in tqdm(range(pages)):
             page_soup = get_page_soup(url.format(page_number=page))
             apartments_links += get_apartments_links(page_soup)
-        with open('apartments_links.csv', 'w') as f:
+            time.sleep(5)
+        with open('apartments_data/apartments_links.csv', 'a+', newline='') as f:
             writer = csv.writer(f)
             writer.writerow([f'Primary: {rooms} is complete'])
             writer.writerow(apartments_links)
@@ -57,12 +61,8 @@ def get_all_apartments_links():
         for page in tqdm(range(pages)):
             page_soup = get_page_soup(url.format(page_number=page))
             apartments_links += get_apartments_links(page_soup)
-        with open('apartments_links.csv', 'w') as f:
+            time.sleep(5)
+        with open('apartments_data/apartments_links.csv', 'a+', newline='') as f:
             writer = csv.writer(f)
             writer.writerow([f'Secondary: {rooms} is complete'])
             writer.writerow(apartments_links)
-
-    return apartments_links
-
-
-apartments_links = get_all_apartments_links()
