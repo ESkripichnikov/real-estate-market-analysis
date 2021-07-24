254e16f83101d56e249d91df70ed5595d605b22d
ESkripichnikov <skrijeka14@gmail.com>
2021-07-24 19:48:52 +0300

    apartments_database collection completed

--- data_collection.py
+++ data_collection.py
@@ -1,4 +1,5 @@
 from parsers.apartments_links_parser import parse_apartments_links
-# from parsers.apartments_info_parser import
+from parsers.apartments_info_parser import parse_apartments_data
 
 parse_apartments_links()
+parse_apartments_data()
