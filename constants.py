4b44f359d748241cb4f5c1fbbbdc4a10d8ac364f
ESkripichnikov <skrijeka14@gmail.com>
2021-07-24 19:50:14 +0300

    constands updated

--- constants.py
+++ constants.py
@@ -6,7 +6,7 @@ secondary = [1000, 9000, 13000, 12000, 4500, 2000, 1000, 500]  # number of apart
 primary = [4200, 10500, 14000, 8000, 2000, 180, 220]  # number of apartments of each type for primary housing
 number_to_parse = 10000
 cian_url = "https://www.cian.ru/cat.php?deal_type=sale&engine_version=2&" \
-           "object_type[0]={category}&offer_type=flat&region=-1&room{rooms_number}=1"
+           "object_type[0]={category}&offer_type=flat&region=1&room{rooms_number}=1"
 
 
 class Category(Enum):
@@ -24,3 +24,26 @@ class Rooms(Enum):
     six = 6
     studio = 9
     other = 7
+
+
+feature_names = ['id', 'Город', 'Округ', 'Район', 'Улица', 'Дом',
+                 'Общая', 'Жилая', 'Кухня', 'Этаж', 'Построен', 'Срок сдачи',
+                 'Тип жилья', 'Высота потолков', 'Санузел', 'Ванная комната', 'Балкон/лоджия',
+                 'Ремонт', 'Вид из окон', 'Площадь комнат', 'Отделка',
+                 'Год постройки', 'Тип дома', 'Тип перекрытий', 'Подъезды', 'Лифты', 'Отопление',
+                 'Аварийность', 'Парковка', 'Мусоропровод', 'Газоснабжение',
+                 'Сдача комплекса', 'Застройщик', 'Класс', 'Кол-во корпусов',
+                 'Ближайшее метро', 'Время до ближайшего метро', 'Цена'
+                 ]
+
+column_names = {'Город': 'city', 'Округ': 'district', 'Район': 'region', 'Улица': 'street', 'Дом': 'house',
+                'Общая': 'total_area', 'Жилая': 'living_area', 'Кухня': 'kitchen_area', 'Этаж': 'floor_number',
+                'Построен': 'build_date', 'Срок сдачи': 'completion_date', 'Тип жилья': 'housing_type',
+                'Высота потолков': 'ceiling_high', 'Санузел': 'bathroom_type', 'Ванная комната': 'bathroom',
+                'Балкон/лоджия': 'balcony', 'Ремонт': 'apartment_renovation', 'Вид из окон': 'window_view',
+                'Площадь комнат': 'rooms_area', 'Отделка': 'apartment_finishing', 'Год постройки': 'house_build_date',
+                'Тип дома': 'house_type', 'Тип перекрытий': 'overlap_type', 'Подъезды': 'entrances', 'Лифты': 'elevators',
+                'Отопление': 'heating', 'Аварийность': 'emergency', 'Парковка': 'parking', 'Мусоропровод': 'garbage chute',
+                'Газоснабжение': 'gass_supply', 'Сдача комплекса': 'complex_completion_date', 'Застройщик': 'developer',
+                'Класс': 'housing_class', 'Кол-во корпусов': 'buldings_number', 'Ближайшее метро': 'nearest_subway',
+                'Время до ближайшего метро': 'nearest_subway_time', 'Цена': 'price'}
